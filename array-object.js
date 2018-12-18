5882a9ab7e97bc15b21ebb7a5e3c43b078572c17
blue2525989 <blue2525989@gmail.com>
2018-12-18 09:28:59 -0600

    files rewritten to run more test in less lines

--- array-object.js
+++ array-object.js
@@ -1,224 +1,164 @@
 'use strict'
 
-function arrayCompare1() {
-  let arrayOne = []
-  let arrayTwo = []
-  let arrayThree = []
-  let arrayFour = []
+const co = require('co')
 
-  for (let i = 0; i < 500; i++) {
-    const obj = {
-      key1: "string1",
-      key2: 22,
-      key3: {
-        inner1: "string",
-        inner2: "value"
+// generator function cannot handle 2000000, most likely added overhead of calling co to yield promise
+const values = [500, 10000, 50000, 100000, 250000, 1000000]
+
+function arrayCompare() {
+
+
+  // iterate through different sizes
+  for (let valIdx = 0; valIdx < values.length; valIdx++) {
+
+    let arrayOne = []
+    let arrayTwo = []
+    let arrayThree = []
+    let arrayFour = []
+
+    for (let i = 0; i < values[valIdx]; i++) {
+      const obj = {
+        key1: "string1",
+        key2: 22,
+        key3: {
+          inner1: "string",
+          inner2: "value"
+        }
       }
+      arrayOne.push(obj)
+      arrayTwo.push(obj)
+      arrayThree.push(obj)
+      arrayFour.push(obj)
     }
-    arrayOne.push(obj)
-    arrayTwo.push(obj)
-    arrayThree.push(obj)
-    arrayFour.push(obj)
+
+    const start1 = new Date()
+    for(let i = 0; i < arrayOne.length; i++) {
+      arrayOne[i].key3.inner1 = "bananas"
+    }
+    const end1 = new Date()
+    // clear
+    arrayOne = null
+
+
+    const start2 = new Date()
+    for(let obj in arrayTwo) {
+      arrayTwo[obj].key3.inner1 = "bananas"
+    }
+    const end2 = new Date()
+    // clear
+    arrayTwo = null
+
+
+    const start3 = new Date()
+    arrayThree.forEach( (obj, idx) => { arrayThree[idx].key3.inner1 = "bananas" })
+    const end3 = new Date()
+    // clear
+    arrayThree = null
+
+
+    const start4 = new Date()
+    arrayFour = arrayFour.map(obj => obj.key3.inner1 = "bananas")
+    const end4 = new Date()
+    // clear
+    arrayFour = null
+
+    console.log(`\n\t*${values[valIdx]}*\n`)
+
+    console.log("for i loop", end1 - start1)
+
+    console.log("for of loop", end2 - start2)
+
+    console.log("forEach loop", end3 - start3)
+
+    console.log("map loop", end4 - start4)
+
+    console.log("\n")
+
   }
 
-  const start1 = new Date()
-  for(let i = 0; i < arrayOne.length; i++) {
-    arrayOne[i].key3.inner1 = "bananas"
-  }
-  const end1 = new Date()
 
-
-  const start2 = new Date()
-  for(let obj in arrayTwo) {
-    arrayTwo[obj].key3.inner1 = "bananas"
-  }
-  const end2 = new Date()
-
-
-  const start3 = new Date()
-  arrayThree.forEach( (obj, idx) => { arrayThree[idx].key3.inner1 = "bananas" })
-  const end3 = new Date()
-
-
-  const start4 = new Date()
-  arrayFour = arrayFour.map(obj => obj.key3.inner1 = "bananas")
-  const end4 = new Date()
-
-  console.log("for i loop", end1 - start1)
-
-  console.log("for of loop", end2 - start2)
-
-  console.log("forEach loop", end3 - start3)
-
-  console.log("map loop", end4 - start4)
 }
 
-function arrayCompare2() {
-  let arrayOne = []
-  let arrayTwo = []
-  let arrayThree = []
-  let arrayFour = []
+function* arrayCompareGenerator() {
 
-  for (let i = 0; i < 50000; i++) {
-    const obj = {
-      key1: "string1",
-      key2: 22,
-      key3: {
-        inner1: "string",
-        inner2: "value"
+  // iterate through different sizes
+  for (let valIdx = 0; valIdx < values.length; valIdx++) {
+
+    let arrayOne = []
+    let arrayTwo = []
+    let arrayThree = []
+    let arrayFour = []
+
+    for (let i = 0; i < values[valIdx]; i++) {
+      const obj = {
+        key1: "string1",
+        key2: 22,
+        key3: {
+          inner1: "string",
+          inner2: "value"
+        }
       }
+      arrayOne.push(obj)
+      arrayTwo.push(obj)
+      arrayThree.push(obj)
+      arrayFour.push(obj)
     }
-    arrayOne.push(obj)
-    arrayTwo.push(obj)
-    arrayThree.push(obj)
-    arrayFour.push(obj)
+
+    const start1 = new Date()
+    for(let i = 0; i < arrayOne.length; i++) {
+      arrayOne[i].key3.inner1 = "bananas"
+    }
+    const end1 = new Date()
+    // clear
+    arrayOne = null
+
+
+    const start2 = new Date()
+    for(let obj in arrayTwo) {
+      arrayTwo[obj].key3.inner1 = "bananas"
+    }
+    const end2 = new Date()
+    // clear
+    arrayTwo = null
+
+
+    const start3 = new Date()
+    arrayThree.forEach( (obj, idx) => { arrayThree[idx].key3.inner1 = "bananas" })
+    const end3 = new Date()
+    // clear
+    arrayThree = null
+
+
+    const start4 = new Date()
+    arrayFour = yield arrayFour.map(function* (obj) { obj.key3.inner1 = "bananas" })
+    const end4 = new Date()
+    // clear
+    arrayFour = null
+
+    console.log(`\n\t*${values[valIdx]}*\n`)
+
+    console.log("for i loop", end1 - start1)
+
+    console.log("for of loop", end2 - start2)
+
+    console.log("forEach loop", end3 - start3)
+
+    console.log("map loop", end4 - start4)
+
+    console.log("\n")
   }
 
-  const start1 = new Date()
-  for(let i = 0; i < arrayOne.length; i++) {
-    arrayOne[i].key3.inner1 = "bananas"
-  }
-  const end1 = new Date()
-
-
-  const start2 = new Date()
-  for(let obj in arrayTwo) {
-    arrayTwo[obj].key3.inner1 = "bananas"
-  }
-  const end2 = new Date()
-
-
-  const start3 = new Date()
-  arrayThree.forEach( (obj, idx) => { arrayThree[idx].key3.inner1 = "bananas" })
-  const end3 = new Date()
-
-
-  const start4 = new Date()
-  arrayFour = arrayFour.map(obj => obj.key3.inner1 = "bananas")
-  const end4 = new Date()
-
-  console.log("for i loop", end1 - start1)
-
-  console.log("for of loop", end2 - start2)
-
-  console.log("forEach loop", end3 - start3)
-
-  console.log("map loop", end4 - start4)
 }
 
-function arrayCompare3() {
-  let arrayOne = []
-  let arrayTwo = []
-  let arrayThree = []
-  let arrayFour = []
 
-  for (let i = 0; i < 100000; i++) {
-    const obj = {
-      key1: "string1",
-      key2: 22,
-      key3: {
-        inner1: "string",
-        inner2: "value"
-      }
-    }
-    arrayOne.push(obj)
-    arrayTwo.push(obj)
-    arrayThree.push(obj)
-    arrayFour.push(obj)
-  }
+/**
+ * run the functions
+ */
 
-  const start1 = new Date()
-  for(let i = 0; i < arrayOne.length; i++) {
-    arrayOne[i].key3.inner1 = "bananas"
-  }
-  const end1 = new Date()
-
-
-  const start2 = new Date()
-  for(let obj in arrayTwo) {
-    arrayTwo[obj].key3.inner1 = "bananas"
-  }
-  const end2 = new Date()
-
-
-  const start3 = new Date()
-  arrayThree.forEach( (obj, idx) => { arrayThree[idx].key3.inner1 = "bananas" })
-  const end3 = new Date()
-
-
-  const start4 = new Date()
-  arrayFour = arrayFour.map(obj => obj.key3.inner1 = "bananas")
-  const end4 = new Date()
-
-  console.log("for i loop", end1 - start1)
-
-  console.log("for of loop", end2 - start2)
-
-  console.log("forEach loop", end3 - start3)
-
-  console.log("map loop", end4 - start4)
-}
-
-function arrayCompare4() {
-  let arrayOne = []
-  let arrayTwo = []
-  let arrayThree = []
-  let arrayFour = []
-
-  for (let i = 0; i < 200000; i++) {
-    const obj = {
-      key1: "string1",
-      key2: 22,
-      key3: {
-        inner1: "string",
-        inner2: "value"
-      }
-    }
-    arrayOne.push(obj)
-    arrayTwo.push(obj)
-    arrayThree.push(obj)
-    arrayFour.push(obj)
-  }
-
-  const start1 = new Date()
-  for(let i = 0; i < arrayOne.length; i++) {
-    arrayOne[i].key3.inner1 = "bananas"
-  }
-  const end1 = new Date()
-
-
-  const start2 = new Date()
-  for(let obj in arrayTwo) {
-    arrayTwo[obj].key3.inner1 = "bananas"
-  }
-  const end2 = new Date()
-
-
-  const start3 = new Date()
-  arrayThree.forEach( (obj, idx) => { arrayThree[idx].key3.inner1 = "bananas" })
-  const end3 = new Date()
-
-
-  const start4 = new Date()
-  arrayFour = arrayFour.map(obj => obj.key3.inner1 = "bananas")
-  const end4 = new Date()
-
-  console.log("for i loop", end1 - start1)
-
-  console.log("for of loop", end2 - start2)
-
-  console.log("forEach loop", end3 - start3)
-
-  console.log("map loop", end4 - start4)
-}
-
-arrayCompare1()
+arrayCompare()
 console.log("\n")
-arrayCompare2()
-console.log("\n")
-arrayCompare3()
-console.log("\n")
-arrayCompare4()
+
+co(function* () { yield arrayCompareGenerator() })
 console.log("\n")
 
 
--- array-prim.js
+++ array-prim.js
@@ -1,208 +1,155 @@
 'use strict'
 
-function arrayComparePrimativeSmall() {
-  let arrayOne = []
-  let arrayTwo = []
-  let arrayThree = []
-  let arrayFour = []
+const co = require('co')
 
-  for (let i = 0; i < 500; i++) {
-    arrayOne.push(true)
-    arrayTwo.push(true)
-    arrayThree.push(true)
-    arrayFour.push(true)
+
+const values = [500, 10000, 50000, 100000, 250000, 1000000]
+
+function arrayCompare() {
+
+
+  // iterate through different sizes
+  for (let valIdx = 0; valIdx < values.length; valIdx++) {
+
+    let arrayOne = []
+    let arrayTwo = []
+    let arrayThree = []
+    let arrayFour = []
+
+    for (let i = 0; i < values[valIdx]; i++) {
+      const obj = {
+        key1: "string1",
+        key2: 22,
+        key3: {
+          inner1: "string",
+          inner2: "value"
+        }
+      }
+      arrayOne.push(obj)
+      arrayTwo.push(obj)
+      arrayThree.push(obj)
+      arrayFour.push(obj)
+    }
+
+    const start1 = new Date()
+    for(let i = 0; i < arrayOne.length; i++) {
+      arrayOne[i].key3.inner1 = "bananas"
+    }
+    const end1 = new Date()
+    // clear
+    arrayOne = null
+
+
+    const start2 = new Date()
+    for(let obj in arrayTwo) {
+      arrayTwo[obj].key3.inner1 = "bananas"
+    }
+    const end2 = new Date()
+    // clear
+    arrayTwo = null
+
+
+    const start3 = new Date()
+    arrayThree.forEach( (obj, idx) => { arrayThree[idx].key3.inner1 = "bananas" })
+    const end3 = new Date()
+    // clear
+    arrayThree = null
+
+
+    const start4 = new Date()
+    arrayFour = arrayFour.map(obj => obj.key3.inner1 = "bananas")
+    const end4 = new Date()
+    // clear
+    arrayFour = null
+
+    console.log(`\n\t*${values[valIdx]}*\n`)
+
+    console.log("for i loop", end1 - start1)
+
+    console.log("for of loop", end2 - start2)
+
+    console.log("forEach loop", end3 - start3)
+
+    console.log("map loop", end4 - start4)
+
+    console.log("\n")
+
   }
 
-  const start1 = new Date()
-  for(let i = 0; i < arrayOne.length; i++) {
-    arrayOne[i] = false
-  }
-  const end1 = new Date()
 
-
-  const start2 = new Date()
-  for(let prim of arrayTwo) {
-    prim = false
-  }
-  const end2 = new Date()
-
-
-  const start3 = new Date()
-  arrayThree.forEach( (prim, idx) => { arrayThree[idx] = false })
-  const end3 = new Date()
-
-
-  const start4 = new Date()
-  arrayFour = arrayFour.map(prim => prim = false)
-  const end4 = new Date()
-
-  console.log("for i loop", end1 - start1)
-  console.log("for i loop", arrayOne[0])
-
-  console.log("for of loop", end2 - start2)
-  console.log("for of loop", arrayTwo[0])
-
-  console.log("forEach loop", end3 - start3)
-  console.log("forEach loop", arrayThree[0])
-
-  console.log("map loop", end4 - start4)
-  console.log("map loop", arrayFour[0])
 }
 
-function arrayComparePrimativeMedium() {
-  let arrayOne = []
-  let arrayTwo = []
-  let arrayThree = []
-  let arrayFour = []
+function* arrayCompareGenerator() {
 
-  for (let i = 0; i < 50000; i++) {
-    arrayOne.push(true)
-    arrayTwo.push(true)
-    arrayThree.push(true)
-    arrayFour.push(true)
+  // iterate through different sizes
+  for (let valIdx = 0; valIdx < values.length; valIdx++) {
+
+    let arrayOne = []
+    let arrayTwo = []
+    let arrayThree = []
+    let arrayFour = []
+
+    for (let i = 0; i < values[valIdx]; i++) {
+      let val = true
+      arrayOne.push(val)
+      arrayTwo.push(val)
+      arrayThree.push(val)
+      arrayFour.push(val)
+    }
+
+    const start1 = new Date()
+    for(let i = 0; i < arrayOne.length; i++) {
+      arrayOne[i] = false
+    }
+    const end1 = new Date()
+    // clear
+    arrayOne = null
+
+
+    const start2 = new Date()
+    for(let obj in arrayTwo) {
+      arrayTwo[obj] = false
+    }
+    const end2 = new Date()
+    // clear
+    arrayTwo = null
+
+
+    const start3 = new Date()
+    arrayThree.forEach( (obj, idx) => { arrayThree[idx] = false })
+    const end3 = new Date()
+    // clear
+    arrayThree = null
+
+
+    const start4 = new Date()
+    arrayFour = yield arrayFour.map(function* (obj) { obj = false; return obj })
+    const end4 = new Date()
+    // clear
+    arrayFour = null
+
+    console.log(`\n\t*${values[valIdx]}*\n`)
+
+    console.log("for i loop", end1 - start1)
+
+    console.log("for of loop", end2 - start2)
+
+    console.log("forEach loop", end3 - start3)
+
+    console.log("map loop", end4 - start4)
+
+    console.log("\n")
   }
 
-  const start1 = new Date()
-  for(let i = 0; i < arrayOne.length; i++) {
-    arrayOne[i] = false
-  }
-  const end1 = new Date()
-
-
-  const start2 = new Date()
-  for(let prim of arrayTwo) {
-    prim = false
-  }
-  const end2 = new Date()
-
-
-  const start3 = new Date()
-  arrayThree.forEach( (prim, idx) => { arrayThree[idx] = false })
-  const end3 = new Date()
-
-
-  const start4 = new Date()
-  arrayFour = arrayFour.map(prim => prim = false)
-  const end4 = new Date()
-
-  console.log("for i loop", end1 - start1)
-  console.log("for i loop", arrayOne[0])
-
-  console.log("for of loop", end2 - start2)
-  console.log("for of loop", arrayTwo[0])
-
-  console.log("forEach loop", end3 - start3)
-  console.log("forEach loop", arrayThree[0])
-
-  console.log("map loop", end4 - start4)
-  console.log("map loop", arrayFour[0])
 }
 
-function arrayComparePrimativeLarge() {
-  let arrayOne = []
-  let arrayTwo = []
-  let arrayThree = []
-  let arrayFour = []
-
-  for (let i = 0; i < 5000000; i++) {
-    arrayOne.push(true)
-    arrayTwo.push(true)
-    arrayThree.push(true)
-    arrayFour.push(true)
-  }
-
-  const start1 = new Date()
-  for(let i = 0; i < arrayOne.length; i++) {
-    arrayOne[i] = false
-  }
-  const end1 = new Date()
-
-
-  const start2 = new Date()
-  for(let prim of arrayTwo) {
-    prim = false
-  }
-  const end2 = new Date()
-
-
-  const start3 = new Date()
-  arrayThree.forEach( (prim, idx) => { arrayThree[idx] = false })
-  const end3 = new Date()
-
-
-  const start4 = new Date()
-  arrayFour = arrayFour.map(prim => prim = false)
-  const end4 = new Date()
-
-  console.log("for i loop", end1 - start1)
-  console.log("for i loop", arrayOne[0])
-
-  console.log("for of loop", end2 - start2)
-  console.log("for of loop", arrayTwo[0])
-
-  console.log("forEach loop", end3 - start3)
-  console.log("forEach loop", arrayThree[0])
-
-  console.log("map loop", end4 - start4)
-  console.log("map loop", arrayFour[0])
-}
-
-function arrayComparePrimativeExtraLarge() {
-  let arrayOne = []
-  let arrayTwo = []
-  let arrayThree = []
-  let arrayFour = []
-
-  for (let i = 0; i < 12500000; i++) {
-    arrayOne.push(true)
-    arrayTwo.push(true)
-    arrayThree.push(true)
-    arrayFour.push(true)
-  }
-
-  const start1 = new Date()
-  for(let i = 0; i < arrayOne.length; i++) {
-    arrayOne[i] = false
-  }
-  const end1 = new Date()
-
-
-  const start2 = new Date()
-  for(let prim of arrayTwo) {
-    prim = false
-  }
-  const end2 = new Date()
-
-
-  const start3 = new Date()
-  arrayThree.forEach( (prim, idx) => { arrayThree[idx] = false })
-  const end3 = new Date()
-
-
-  const start4 = new Date()
-  arrayFour = arrayFour.map(prim => prim = false)
-  const end4 = new Date()
-
-  console.log("for i loop", end1 - start1)
-  console.log("for i loop", arrayOne[0])
-
-  console.log("for of loop", end2 - start2)
-  console.log("for of loop", arrayTwo[0])
-
-  console.log("forEach loop", end3 - start3)
-  console.log("forEach loop", arrayThree[0])
-
-  console.log("map loop", end4 - start4)
-  console.log("map loop", arrayFour[0])
-}
-
-arrayComparePrimativeSmall()
+/**
+ * run the jewels
+ */
+arrayCompare()
 console.log("\n")
-arrayComparePrimativeMedium()
-console.log("\n")
-arrayComparePrimativeLarge()
-console.log("\n")
-arrayComparePrimativeExtraLarge()
+
+co( function* () { yield arrayCompareGenerator() } )
 console.log("\n")
 
 
