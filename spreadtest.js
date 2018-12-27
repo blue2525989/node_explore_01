83c901cf999de8e648ab55f8ab6147f396c1d06a
blue2525989 <blue2525989@gmail.com>
2018-12-27 09:20:51 -0600

    spread operator

---
+++ spreadtest.js
@@ -0,0 +1,28 @@
+'use strict'
+
+function spread(...words) {
+  if (!(words.length % 2 == 0)) {
+    return 'must be divisible by two to ensure proper key/value building.'
+  }
+  const response = []
+  let tempObj = {}
+  let valuePlaced = false
+  for (let i = 0; i < words.length; i++) {
+    if (i % 2 == 0) {
+      tempObj.name = words[i]
+    } else {
+      tempObj.value = words[i]
+      valuePlaced = true
+    }
+    if (valuePlaced) {
+      response.push(tempObj)
+      valuePlaced = false
+      tempObj = new Object()
+    }
+  }
+  return response
+}
+
+const response = spread('namey', 'valuey', 'meowy', 'kitty', 'woofy', 'doggy')
+
+console.log(response)
